983f35b4fdbeaa4f7267afc4d886e7e9ce88b626
Vixitonian <hachugasim@gmail.com>
2024-05-06 10:24:27 +0100

    first commit

---
+++ hello_world.dart
@@ -0,0 +1,26 @@
+void main() {
+  /// info variable
+
+  int info = 10;
+  String message = "CHinedu"; //this variable stores chinedu
+  String school = 'Unizik';
+  String bio23453 = """
+  Chinedu
+  Ebuka
+  Fidelis
+  Chidera""";
+// this part of the code prints all the variables
+  print(info);
+  print(school); //this prints the school name
+
+  /* 
+    This is my first line comments
+    this is my second line comment 
+    this i smy third line comment 
+    but there is nothing wrong with me typing it, 
+    i am very fast
+    and i dont know what to do
+  */
+
+  //print(bio);
+}
---
+++ playground.dart
@@ -0,0 +1,3 @@
+void main() {
+  print('Playground file');
+}
